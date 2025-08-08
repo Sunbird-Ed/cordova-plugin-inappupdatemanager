c3acafa5835633943c9ac52984c34dd225111d8c
Deeksha Deepak <deekshad1502@gmail.com>
2025-08-08 15:10:57 +0530

    Update: Migrate to new Play Core libraries

--- src/android/InAppUpdateManager.java
+++ src/android/InAppUpdateManager.java
@@ -3,12 +3,12 @@ package InAppUpdateManager;
 import android.content.Context;
 import android.content.IntentSender;
 
-import com.google.android.play.core.appupdate.AppUpdateInfo;
-import com.google.android.play.core.appupdate.AppUpdateManager;
-import com.google.android.play.core.appupdate.AppUpdateManagerFactory;
-import com.google.android.play.core.install.model.AppUpdateType;
-import com.google.android.play.core.install.model.UpdateAvailability;
-import com.google.android.play.core.tasks.Task;
+import com.google.android.play.app.update.AppUpdateInfo;
+import com.google.android.play.app.update.AppUpdateManager;
+import com.google.android.play.app.update.AppUpdateManagerFactory;
+import com.google.android.play.app.update.model.AppUpdateType;
+import com.google.android.play.app.update.model.UpdateAvailability;
+import com.google.android.gms.tasks.Task;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
